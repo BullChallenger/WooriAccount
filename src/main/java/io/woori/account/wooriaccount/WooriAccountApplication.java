336cd662a3773abc29b5f48f185d7e4f22f41833
BullChallenger <am23am45@naver.com>
2023-12-29 17:44:19 +0900

    Init Commit

---
+++ src/main/java/io/woori/account/wooriaccount/WooriAccountApplication.java
@@ -0,0 +1,13 @@
+package io.woori.account.wooriaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooriAccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WooriAccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/woori/account/wooriaccount/WooriAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package io.woori.account.wooriaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WooriAccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
