afb3789116fbacbf322750493b86602722a312f7
hwiju yeom <yeom456@gmail.com>
2023-12-30 20:09:58 +0900

    feat: querydsl을 사용한 Account 레포지토리 인터페이스 작성

---
+++ src/main/java/io/woori/account/wooriaccount/repository/querydsl/inter/QueryAccountRepository.java
@@ -0,0 +1,9 @@
+package io.woori.account.wooriaccount.repository.querydsl.inter;
+
+import io.woori.account.wooriaccount.domain.entity.Account;
+
+public interface QueryAccountRepository {
+
+    Account queryFindById(Long id);
+
+}
