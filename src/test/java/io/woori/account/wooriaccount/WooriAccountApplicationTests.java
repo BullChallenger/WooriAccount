fe0c385bf59ae2d3b24c60b4ac17d694b4f5e752
hwiju yeom <yeom456@gmail.com>
2023-12-30 20:09:28 +0900

    Update WooriAccountApplicationTests.java

--- src/test/java/io/woori/account/wooriaccount/WooriAccountApplicationTests.java
+++ src/test/java/io/woori/account/wooriaccount/WooriAccountApplicationTests.java
@@ -1,13 +1,13 @@
-package io.woori.account.wooriaccount;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class WooriAccountApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package io.woori.account.wooriaccount;
+//
+////import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+////@SpringBootTest
+//class WooriAccountApplicationTests {
+//
+//   // @Test
+//    void contextLoads() {
+//    }
+//
+//}
