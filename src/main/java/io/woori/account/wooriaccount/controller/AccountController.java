235674032384dbd0a1fe4c5b27560168c7948469
hwiju yeom <yeom456@gmail.com>
2024-01-02 17:45:42 +0900

    Create AccountController.java

---
+++ src/main/java/io/woori/account/wooriaccount/controller/AccountController.java
@@ -0,0 +1,12 @@
+package io.woori.account.wooriaccount.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+}
