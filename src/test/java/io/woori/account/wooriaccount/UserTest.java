4d9f32ede4e363b3f0f64909ca7b750b3f94ab0d
Dumbveloper <qhdtjq4799@naver.com>
2024-01-02 11:15:03 +0900

    testcode edit by Song

--- src/test/java/io/woori/account/wooriaccount/UserTest.java
+++ src/test/java/io/woori/account/wooriaccount/UserTest.java
@@ -5,54 +5,53 @@ import io.woori.account.wooriaccount.dto.user.LoginResponseDTO;
 import io.woori.account.wooriaccount.dto.user.SignUpRequestDTO;
 import io.woori.account.wooriaccount.exception.CustomException;
 import io.woori.account.wooriaccount.service.CustomerService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 @Transactional
-@RunWith(SpringRunner.class)
 public class UserTest {
 
     @Autowired
     private CustomerService customerService;
 
     @Test
+    @Rollback
     public void 회원가입(){
         //given
         SignUpRequestDTO dto = new SignUpRequestDTO("봉", "010-1234-1234", "abc@naver.com", "1234");
         //when
         String result = customerService.signUp(dto);
         //then
-        assertEquals("success",result);
+        assertEquals("success", result);
     }
 
     @Test
+    @Rollback
     public void 로그인(){
         SignUpRequestDTO dto = new SignUpRequestDTO("봉", "010-1234-1234", "abc@naver.com", "1234");
         customerService.signUp(dto);
 
         LoginRequestDTO loginRequestDTO = new LoginRequestDTO("abc@naver.com", "1234");
-        LoginResponseDTO loginResponseDTO =  customerService.login(loginRequestDTO);
+        LoginResponseDTO loginResponseDTO = customerService.login(loginRequestDTO);
         System.out.println(loginResponseDTO.getCustomerEmail());
-
     }
 
     @Test
+    @Rollback
     public void 로그인시도_오류() throws Exception{
         //given
         SignUpRequestDTO dto = new SignUpRequestDTO("봉", "010-1234-1234", "abc@naver.com", "1234");
         customerService.signUp(dto);
 
         // when
-        RuntimeException exception = assertThrows(CustomException.class, () -> {
+        CustomException exception = assertThrows(CustomException.class, () -> {
             LoginRequestDTO loginRequestDTO = new LoginRequestDTO("def@naver", "1234"); // 잘못된 이메일로 로그인 시도
             customerService.login(loginRequestDTO);
         });
@@ -60,7 +59,9 @@ public class UserTest {
         // then
         assertEquals("존재하지 않는 고객 정보입니다.", exception.getMessage());
     }
+
     @Test
+    @Rollback
     public void 중복회원가입_오류(){
         SignUpRequestDTO dto1 = new SignUpRequestDTO("봉", "010-1234-1234", "abc@naver.com", "1234");
         SignUpRequestDTO dto2 = new SignUpRequestDTO("섭", "010-5678-5678", "abc@naver.com", "5678");
@@ -74,6 +75,4 @@ public class UserTest {
         // then
         assertEquals("중복된 이메일입니다.", exception.getMessage());
     }
-
-
 }
