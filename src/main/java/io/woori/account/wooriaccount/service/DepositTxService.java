4ccb761dd098c440fd758ff21c26052955ef6901
hwiju yeom <yeom456@gmail.com>
2024-01-02 12:41:36 +0900

    Update DepositTxService.java

--- src/main/java/io/woori/account/wooriaccount/service/DepositTxService.java
+++ src/main/java/io/woori/account/wooriaccount/service/DepositTxService.java
@@ -1,9 +1,8 @@
 package io.woori.account.wooriaccount.service;
 
-import io.woori.account.wooriaccount.domain.entity.DepositTxHistory;
-import io.woori.account.wooriaccount.repository.jpa.DepositTxHistoryRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
