68ed727b652ff481e62c2e0885819d28afdec92a
Dumbveloper <qhdtjq4799@naver.com>
2024-01-02 17:12:42 +0900

    CookieUtil

--- src/main/java/io/woori/account/wooriaccount/controller/CustomerController.java
+++ src/main/java/io/woori/account/wooriaccount/controller/CustomerController.java
@@ -4,8 +4,11 @@ import io.woori.account.wooriaccount.dto.user.LoginRequestDTO;
 import io.woori.account.wooriaccount.dto.user.LoginResponseDTO;
 import io.woori.account.wooriaccount.dto.user.SignUpRequestDTO;
 import io.woori.account.wooriaccount.service.inter.CustomerService;
+import io.woori.account.wooriaccount.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,8 +17,12 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO) {
-        return customerService.login(loginRequestDTO);
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO loginRequestDTO , HttpSession session) {
+        LoginResponseDTO loginResponseDTO = customerService.login(loginRequestDTO);
+
+        session.setAttribute("customerId", loginResponseDTO);
+
+        return loginResponseDTO;
     }
 
     public String signUp(@RequestBody SignUpRequestDTO signUpRequestDTO) {
@@ -25,5 +32,4 @@ public class CustomerController {
     public String delete(@RequestParam(name = "customerId") Long customerId) {
         return customerService.delete(customerId);
     }
-
 }
