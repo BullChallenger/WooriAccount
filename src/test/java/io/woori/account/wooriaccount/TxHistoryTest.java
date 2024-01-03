62e3f090432dc0e9cbee354a5dd990dc4cae5629
BullChallenger <am23am45@naver.com>
2024-01-03 10:02:58 +0900

    [FEAT & TEST] Lock 기능 추가 및 테스트 작성

--- src/test/java/io/woori/account/wooriaccount/TxHistoryTest.java
+++ src/test/java/io/woori/account/wooriaccount/TxHistoryTest.java
@@ -4,17 +4,26 @@ import io.woori.account.wooriaccount.domain.entity.AbstractTxHistory;
 import io.woori.account.wooriaccount.domain.entity.Account;
 import io.woori.account.wooriaccount.domain.entity.Customer;
 import io.woori.account.wooriaccount.domain.entity.DepositTxHistory;
+import io.woori.account.wooriaccount.dto.account.AccountRemittanceDTO;
 import io.woori.account.wooriaccount.repository.jpa.AccountRepository;
 import io.woori.account.wooriaccount.repository.jpa.CustomerRepository;
 import io.woori.account.wooriaccount.repository.jpa.TxHistoryRepository;
+import io.woori.account.wooriaccount.repository.querydsl.QueryTransactionHistoryRepositoryImpl;
+import io.woori.account.wooriaccount.service.AccountServiceImpl;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @SpringBootTest
 public class TxHistoryTest {
@@ -26,7 +35,13 @@ public class TxHistoryTest {
     AccountRepository accountRepository;
 
     @Autowired
-    TxHistoryRepository depositTxHistoryRepository;
+    TxHistoryRepository<DepositTxHistory> depositTxHistoryRepository;
+
+    @Autowired
+    QueryTransactionHistoryRepositoryImpl queryTransactionHistoryRepository;
+
+    @Autowired
+    AccountServiceImpl accountService;
 
     @Autowired
     EntityManager entityManager;
@@ -53,6 +68,31 @@ public class TxHistoryTest {
 
 //        AbstractTxHistory byId = depositTxHistoryRepository.findById(1L).orElseThrow();
 //        Assertions.assertEquals(byId.getClass(), DepositTxHistory.class);
+    }
+
+    @Test
+    @Transactional
+    @DisplayName(value = "비관적 락을 걸어 동시성 문제 해소")
+    void txLockTest() throws InterruptedException {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+        AccountRemittanceDTO dto = new AccountRemittanceDTO("ACC123456", "52862517", "송금송금", "5");
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    accountService.accountRemittance(dto);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+        Account acc123456 = accountRepository.findByAccountNumber("ACC123456").orElseThrow();
+        System.out.println(acc123456.getAccountBalance());
+        Assertions.assertEquals(0, BigDecimal.valueOf(4500).compareTo(acc123456.getAccountBalance()));
     }
 
 }
