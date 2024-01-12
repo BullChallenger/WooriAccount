2660256ced9199ea63cba1376726ff52e11cbab0
hwiju yeom <yeom456@gmail.com>
2024-01-12 10:52:14 +0900

    feat: 로그인 인증 과정 중 올바르지 않은 회원 정보를 받았을 때 사용하는 Error code 정의

--- src/main/java/io/woori/account/wooriaccount/exception/ErrorCode.java
+++ src/main/java/io/woori/account/wooriaccount/exception/ErrorCode.java
@@ -16,6 +16,7 @@ public enum ErrorCode implements EnumModel{
     //회원 로그인 시 발생 가능 예외
     INVALID_Customer_Login(401, "C001", "존재하지 않는 고객 정보입니다."),
     INVALID_Customer_Password(401, "C002", "비밀번호가 일치하지 않습니다."),
+    INVALID_CUSTOMER_LOGIN_INFO(401, "C003", "올바르지 않은 로그인 회원 정보입니다."),
 
     //중복여부 체크
     DUPLICATE_CUSTOMER(400,"C003","중복된 이메일입니다."),
