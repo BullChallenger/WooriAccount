8c9fe426e473291222ffcffaabbd08701148fa3d
hwiju yeom <yeom456@gmail.com>
2024-01-18 21:11:21 +0900

    Update WebConfig.java

--- src/main/java/io/woori/account/wooriaccount/configuration/WebConfig.java
+++ src/main/java/io/woori/account/wooriaccount/configuration/WebConfig.java
@@ -1,22 +1,22 @@
-package io.woori.account.wooriaccount.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebMvc
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-	@Override
-	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedOrigins("*")
-			.allowedMethods("*")
-			.allowedHeaders("*")
-			.exposedHeaders("*")
-			.allowCredentials(true)
-			.maxAge(3000);
-	}
-
-}
+//package io.woori.account.wooriaccount.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@EnableWebMvc
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**")
+//			.allowedOrigins("*")
+//			.allowedMethods("*")
+//			.allowedHeaders("*")
+//			.exposedHeaders("*")
+//			.allowCredentials(true)
+//			.maxAge(3000);
+//	}
+//
+//}
